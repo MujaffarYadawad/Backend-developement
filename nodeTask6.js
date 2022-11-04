b6196ba4a87a69638cfaec53eeea39bbb130f070
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2022-11-04 15:13:32 +0530

    printing Name

---
+++ nodeTask6.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res)=> {
+
+  const url = req.url;
+  const method = req.method;
+
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1> Mujaffar </h1></script></body>');
+  res.write('</html>');
+  res.end();
+ 
+  });
+server.listen(4000);
