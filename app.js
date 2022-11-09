364d2d890a3d292a13f088e6c30fae5d627ef11b
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2022-11-09 16:24:27 +0530

    /admin/add-product with seperate admin , shop file and app file

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the Middleware');
-  next(); // allows the request to continue to the next middleware in the line
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-  console.log('In another Middleware');
-  res.send('<h1>Hello from express!</h1>');
- 
+app.use(bodyParser.urlencoded({extended: false}));
 
-});
- 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-// const server = http.createServer(app);
+app.use((req, res , next) => {
+  res.status(404).send('<h1>Page Note Found</h1>')
+})
  
-// server.listen(3000);
 app.listen(3000);
