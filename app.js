71995681dc83c815c10a493840c229ad889160b2
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2023-06-03 16:29:44 +0530

    products to  cart

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("647860ea6863035436dafea4")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
