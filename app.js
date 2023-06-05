90cef45d7ff8fda0f5029ac4ebd3aa6b41078775
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2023-06-05 13:18:23 +0530

    getting order placed

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("647860ea6863035436dafea4")
-    .then((user) => {
+  User.findById('647860ea6863035436dafea4')
+    .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
